6d1202c1f7481abaa66e258ed7e3fcf7aaeac606
Santiago <dasanti_12@hotmail.com>
2022-07-04 15:54:14 -0500

    Fixing helm download

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -9,7 +9,7 @@ echo "Installing Helm..."
 curl -LO "https://get.helm.sh/helm-v$HELM_VERSION-linux-amd64.tar.gz"
 sudo mkdir -p "/usr/local/helm-v$HELM_VERSION"
 sudo tar -xzf "helm-v$HELM_VERSION-linux-amd64.tar.gz" -C "/usr/local/helm-v$HELM_VERSION"
-sudo ln -s "/usr/local/helm-v$HELM_VERSION/linux-amd64/helm" /usr/local/bin/helm
+sudo ln -sfn "/usr/local/helm-v$HELM_VERSION/linux-amd64/helm" /usr/local/bin/helm
 rm -f "helm-v$HELM_VERSION-linux-amd64.tar.gz"
 helm init --client-only
 
